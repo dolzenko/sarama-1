f453d2784530ce41056aa82e80d8559509440ebb
Ev Dolzhenko <evgeniy.dolzhenko@blacksquaremedia.com>
2014-11-21 15:07:24 +0300

    Better rollback test

--- cluster/partition_consumer_test.go
+++ cluster/partition_consumer_test.go
@@ -17,7 +17,7 @@ var _ = Describe("PartitionConsumer", func() {
 		Expect(err).NotTo(HaveOccurred())
 		subject, err = NewPartitionConsumer(client, testConsumerConfig(), t_TOPIC, t_GROUP, 3, 0)
 		Expect(err).NotTo(HaveOccurred())
-		Eventually(func() int { return len(subject.stream.Events()) }).Should(BeNumerically(">", 10))
+
 	})
 
 	AfterEach(func() {
@@ -31,26 +31,42 @@ var _ = Describe("PartitionConsumer", func() {
 		}
 	})
 
-	It("should fetch batches of events", func() {
+	nextBatch := func() *EventBatch {
+		Eventually(func() int { return len(subject.stream.Events()) }).Should(BeNumerically(">", 10))
 		batch := subject.Fetch()
 		Expect(batch).NotTo(BeNil())
+		return batch
+	}
+
+	It("should fetch batches of events", func() {
+		batch := nextBatch()
 		Expect(batch.Topic).To(Equal(t_TOPIC))
 		Expect(batch.Partition).To(Equal(int32(3)))
 		Expect(len(batch.Events)).To(BeNumerically(">", 10))
 	})
 
-	It("should rollback", func() {
-		batch := subject.Fetch()
-		Expect(batch).NotTo(BeNil())
-		Expect(len(batch.Events)).To(BeNumerically(">", 10))
+	It("should rollback to given offset", func() {
+		// Consume something to get real offset
+		nextBatch()
 
+		// Consume the batch to be rolled back to
 		was := subject.Offset()
-		Eventually(func() int { return len(subject.stream.Events()) }).Should(BeNumerically(">", 10))
-		batch = subject.Fetch()
-		Expect(batch).NotTo(BeNil())
+		Expect(was).To(BeNumerically(">", 0))
+		nextBatch()
 
+		// Rollback
 		subject.Rollback(was)
 		Expect(subject.Offset()).To(Equal(was))
+
+		// Confirm consumption starts from given offset
+		batch := nextBatch()
+		min := batch.Events[0].Offset
+		for _, e := range batch.Events {
+			if e.Offset < min {
+				min = e.Offset
+			}
+		}
+		Expect(min).To(Equal(was))
 	})
 
 	It("should close consumer", func() {
